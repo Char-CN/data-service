90a6a56b3474f07026daef480fb8869b7d4bfc0f
hyy <hyy@192.168.99.1>
2016-08-16 18:21:58 +0800

    commit

--- src/main/java/org/blazer/dataservice/interceptor/WebRequestInterceptor.java
+++ src/main/java/org/blazer/dataservice/interceptor/WebRequestInterceptor.java
@@ -3,12 +3,20 @@ package org.blazer.dataservice.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.blazer.dataservice.util.IPUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class WebRequestInterceptor extends HandlerInterceptorAdapter {
 
+	private static Logger logger = LoggerFactory.getLogger("access");
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		logger.debug("IP : " + IPUtil.getIpAddr(request));
+		logger.debug("Query String: " + request.getQueryString());
+		logger.debug("Request URL: " + request.getRequestURL());
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		return super.preHandle(request, response, handler);
 	}
---
+++ src/main/java/org/blazer/dataservice/util/IPUtil.java
@@ -0,0 +1,30 @@
+package org.blazer.dataservice.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IPUtil {
+
+	/**
+	 * 获取登录用户IP地址
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public static String getIpAddr(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		if (ip.equals("0:0:0:0:0:0:0:1")) {
+			ip = "localhost";
+		}
+		return ip;
+	}
+
+}
