2690f023b5ceaf11a6c2acf80b90e699643c88b6
hyy <hyy@172.16.8.15>
2017-07-14 12:17:30 +0800

    修改本地测试源

--- script/config.sh
+++ script/config.sh
@@ -18,7 +18,7 @@ data_service_util_jar="data-service-util-0.0.1-SNAPSHOT.jar";
 
 email_util="email_util.py";
 
-conn="${mysql_path} -h172.16.52.137 -uroot -p123456 -Ddw_dataservice -N -e";
+conn="${mysql_path} -h127.0.0.1 -uroot -proot -Ddw_dataservice -N -e";
 
 get_mails_url="http://bigdata.blazer.org:8030/userservice/getmailsbyuserids.do";
 
